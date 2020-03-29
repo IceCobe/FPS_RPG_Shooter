d16040005ac0a2756caa71ee4e60a5774aba3d50
IceCobe <jaalaba@gmail.com>
2020-03-29 04:12:05 -0400

    Code Clean Up
    UI stuff

--- New FPS/Assets/Player/Scripts/Ammo_Count.cs
+++ New FPS/Assets/Player/Scripts/Ammo_Count.cs
@@ -6,18 +6,7 @@ using UnityEngine.UI;
 public class Ammo_Count : MonoBehaviour
 {
     public Text ammo;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    
     public void changetext(string current) {
         ammo.text = current;
     }
--- New FPS/Assets/Player/Scripts/Round_Script.cs
+++ New FPS/Assets/Player/Scripts/Round_Script.cs
@@ -6,17 +6,6 @@ using UnityEngine.UI;
 public class Round_Script : MonoBehaviour
 {
     public Text round;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void changetext(string current) {
         round.text = current;
