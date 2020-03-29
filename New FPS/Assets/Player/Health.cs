870cc0e6f6ec30971b420b152bfc06e7b5a240e4
IceCobe <jaalaba@gmail.com>
2020-03-29 03:57:20 -0400

    Code Cleanup
    Clean up of Player_movements and health

--- New FPS/Assets/Player/Health.cs
+++ New FPS/Assets/Player/Health.cs
@@ -4,35 +4,42 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Health : MonoBehaviour
-{
+{   
+    public HealthBarScript healthbar;
     public float max_health = 100f;
     public float current_health = 100f;
-    public HealthBarScript healthbar;
-    // Start is called before the first frame update
+    
     void Start()
     {
+        // Set the current_health to max_health
         current_health = max_health;
+
+        // Tell healthbar to set it's max health
         healthbar.SetMaxHealth(max_health);
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // Tell healthbar to match the health here
         healthbar.SetHealth(current_health);
 
+        // If I die then reload the room
         if (current_health <= 0) {
             SceneManager.LoadScene("First_Room");
         }
 
-        if (transform.position.y <= -10) {
+        // If I fall off the map reload the room
+        if (transform.position.y <= -100) {
             SceneManager.LoadScene("First_Room");
         } 
     }
 
+    // Take damage script
     void TakeDamage(float damage) {
         current_health -= damage;
     }
 
+    // If an enemy collides with me destroy it and then take damage
     private void OnTriggerEnter(Collider other) {
         if (other.gameObject.tag == "Enemy" ) {
             Target enemy = other.GetComponent<Target>();
--- New FPS/Assets/Player/Player_Movement.cs
+++ New FPS/Assets/Player/Player_Movement.cs
@@ -6,36 +6,42 @@ public class Player_Movement : MonoBehaviour
 {
 
     public CharacterController controller;
-    public float speed = 12f;
-    public float gravity = -9.81f;
-    public float jumpHeight = 3f;
     public Transform groundCheck;
-    public float groundDistance = 0.2f;
     public LayerMask groundMask;
+
+    public float speed = 12f;
+    public float jumpHeight = 3f;
+    public float gravityMultiplier = 2f;
+    public float groundDistance = 0.2f;
+    
     Vector3 velocity;
     bool isGrounded;
 
-    // Update is called once per frame
     void Update()
     {
+        // Checks to see if we're on the ground
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
+        // If we're on the ground stay there
         if(isGrounded && velocity.y < 0) {
             velocity.y = -2f;
         }
 
+        // Variable gathering
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
+        // Move the character
         Vector3 move = transform.right * x + transform.forward * z;
-
         controller.Move(move * speed * Time.deltaTime);
 
-        if(Input.GetButtonDown("Jump") && isGrounded) {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        // Jump Implementation
+        if (Input.GetButtonDown("Jump") && isGrounded) {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravityMultiplier * -9.81f);
         }
 
-        velocity.y += gravity * Time.deltaTime;
+        // Gravity
+        velocity.y += gravityMultiplier * -9.81f * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
     }
 }
