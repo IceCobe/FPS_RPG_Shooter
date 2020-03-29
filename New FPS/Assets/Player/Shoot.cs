2347aa04238164eba282a3d791b4ed91f3f7d3a0
IceCobe <jaalaba@gmail.com>
2020-03-29 04:05:49 -0400

    Code Cleanup
    Cleaned up slerptolookat and shoot.
    
    also fixed the reloading animation

--- New FPS/Assets/Player/Shoot.cs
+++ New FPS/Assets/Player/Shoot.cs
@@ -6,28 +6,26 @@ public class Shoot : MonoBehaviour
 {
 
     public GameObject bullet;
-    public float speed = 100f;
-
     public GameObject emitter;
+    public Ammo_Count ammo_counter;
 
+    public float speed = 100f;
     public float max_ammo = 8f;
     public float cur_ammo = 8f;
-
     public float reload_time = 1f;
 
-    public Ammo_Count ammo_counter;
     bool isReloading = false;    
-    // Update is called once per frame
-    void Start() {
-    }
+
     void Update()
     {   
+        // tried to implement reloading animation
         if (isReloading) {
             emitter.transform.localPosition = Vector3.Lerp(emitter.transform.localPosition,new Vector3(1.426f, -1.44f, 2.554f), 10f*Time.deltaTime);
         } else {
-            // emitter.transform.localPosition = Vector3.Lerp(emitter.transform.localPosition,new Vector3(1.426f, -0.44f, 2.554f), 10f*Time.deltaTime);
+            emitter.transform.localPosition = Vector3.Lerp(emitter.transform.localPosition,new Vector3(1.426f, -0.44f, 2.554f), 10f*Time.deltaTime);
         }
         
+        // Logic for when I can shoot
         if (cur_ammo <= max_ammo && isReloading == false && (Input.GetKeyDown(KeyCode.R) || cur_ammo <= 0)) {
             isReloading = true;
             Invoke("reload_now", reload_time);
@@ -35,15 +33,17 @@ public class Shoot : MonoBehaviour
             shoot();
         }
 
+        // Update the ammo
         ammo_counter.changetext(cur_ammo+"/"+max_ammo);
     }
 
+    // Reloading implementation
     void reload_now() {
         cur_ammo = max_ammo;
-        emitter.transform.localPosition = new Vector3(1.426f, -0.44f, 2.554f);
         isReloading = false;
     }
 
+    // Shooting implementation
     void shoot(){
         if (Input.GetMouseButtonDown(0)) {
             GameObject instBullet = Instantiate(bullet, transform.position , transform.rotation) as GameObject;
--- New FPS/Assets/Player/SlerpToLookAt.cs
+++ New FPS/Assets/Player/SlerpToLookAt.cs
@@ -2,31 +2,30 @@
  
  public class SlerpToLookAt: MonoBehaviour
  {
-     //values that will be set in the Inspector
-     Transform Target;
-     public Camera fpsCam;
-     public float RotationSpeed;
- 
-     //values for internal use
-     private Quaternion _lookRotation;
-     private Vector3 _direction;
-     
-     // Update is called once per frame 
-     void Update()
-     {
+    public Camera fpsCam;
+    Transform Target;
+
+    public float RotationSpeed;
+    private Quaternion _lookRotation;
+    private Vector3 _direction;
+
+    void Update()
+    {
         RaycastHit hit;
 
-        //find the vector pointing from our position to the target
+        // find the vector pointing from our position to the target
         _direction = fpsCam.transform.forward;
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit)) {
-
             Vector3 Target = hit.point;
-            //find the vector pointing from our position to the target
+
+            // find the vector pointing from our position to the target
             _direction = (Target - transform.position).normalized;
         }
-        //create the rotation we need to be in to look at the target
+
+        // create the rotation we need to be in to look at the target
         _lookRotation = Quaternion.LookRotation(_direction);
-        //rotate us over time according to speed until we are in the required rotation
+
+        // rotate us over time according to speed until we are in the required rotation
         transform.rotation = Quaternion.Slerp(transform.rotation, _lookRotation, Time.deltaTime * RotationSpeed);
      }
  }
