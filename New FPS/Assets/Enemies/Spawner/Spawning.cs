fd991208616b1fde3500be844fa8f9ed9089f31f
IceCobe <jaalaba@gmail.com>
2020-04-05 21:42:46 -0400

    Feature Update - Spawner
    Creatures no longer spawn withint a set distance from player

--- New FPS/Assets/Enemies/Spawner/Spawning.cs
+++ New FPS/Assets/Enemies/Spawner/Spawning.cs
@@ -6,8 +6,12 @@ public class Spawning : MonoBehaviour
 {
     public GameObject enemy;
     public Round_Script round_Script;
+    GameObject player;
     public int round = 0;
 
+    void Start() {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
     void Update()
     {
         // Spawn an enemy
@@ -15,6 +19,9 @@ public class Spawning : MonoBehaviour
             round_Script.changetext("Round " + round);
             for (int i = 0; i < round; i++) {
                 Vector3 position = new Vector3(Random.Range(-50, 50), 5, Random.Range(-50, 50));
+                while (Vector3.Distance(player.transform.position, position) < 25) {
+                    position = new Vector3(Random.Range(-50, 50), 5, Random.Range(-50, 50));
+                }
                 Instantiate(enemy, position, Quaternion.identity);
             }
             round++;
